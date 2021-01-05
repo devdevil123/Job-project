86077cb9c6c6b666cc0b1b3b7910343835663ff6
devdevil123 <39667579+devdevil123@users.noreply.github.com>
2021-01-05 14:54:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>PROJECT</title>
+
+   
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,500&display=swap" rel="stylesheet">
+  
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">DEVENDRA DUBEY</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+     
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">about</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="userinfo.php">services</a>
+      </li>
+      
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="pic/image1.jpg" alt="MUMBAI GATE" width="1570" height="500">
+      <div class="carousel-caption">
+        <h3>MUMBAI GATE</h3>
+        <p>SUCH A BEAUTIFUL ARCHITECTURE</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="pic/image2.jpg" alt="NARIMAN POINT" width="1570" height="500">
+      <div class="carousel-caption">
+        <h3>NARIMAN POINT</h3>
+        <p>GREAT SEA VIEW FOR PEOPLES</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="pic/image3.jpg" alt="MUMBAI BRIDGE" width="1570" height="500">
+      <div class="carousel-caption">
+        <h3>MUMBAI BRIDGE</h3>
+        <p>We love it</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<section class="my-5">
+     <div class="py-5">
+     <h2 class="text-center"> About Us <h2>   
+       </div>  
+
+       <div class="container-fluid">
+       <div class="row">
+            <div class="col-lg-6 col-md-6 col-12">
+            <img src="pic/image3.jpg" class="img-fluid " >
+            </div>
+            <div class="col-lg-6 col-md-6 col-12">
+            <h2 class="display-4"> Amchi Mumbai</h2>
+            <p class="py-3">The city was named after the temple, Mumbadevi. 
+            It is one of the oldest temples in the city and is dedicated to the Goddess Mumbā
+             who is the city’s patron deity.
+            S It is located in Bori Bunder in Mumbai.</p>
+            <a href="about.php" class="btn btn-sucsess">Know More </a>
+            </div>
+            </div>
+            </div>  
+            </section>   
+
+<section class="my-5">
+     <div class="py-5">
+     <h2 class="text-center"> Our Services<h2> 
+     </div>  
+     <div class="container-fluid">
+       <div class="row">
+          <div class="col-lg-4 col-md-4 col-12">
+          <div class="card" >
+  <img class="card-img-top" src="pic/image2.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">somthing to write</h4>
+    <p class="card-text">likh idhr kuch bhi.</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+          <div class="card" >
+  <img class="card-img-top" src="pic/image2.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">somthing to write</h4>
+    <p class="card-text">likh idhr kuch bhi.</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+</div>
+
+
+<div class="col-lg-4 col-md-4 col-12">
+          <div class="card" >
+  <img class="card-img-top" src="pic/image2.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">somthing to write</h4>
+    <p class="card-text">likh idhr kuch bhi.</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+</div>
+
+</div>
+</div>
+</section>
+
+
+
+
+  
+<section class="my-5">
+     <div class="py-5">
+     <h2 class="text-center"> Our Gallery<h2> 
+     </div>  
+     <div class="container-fluid">
+     <div class="row">
+     <div class="col-lg-4 col-md-4 col-12">
+     <img src="pic/image3.jpg" class="img-fluid pb-4">
+     </div>
+     <div class="col-lg-4 col-md-4 col-12">
+     <img src="pic/image3.jpg" class="img-fluid pb-4">
+     </div>
+     <div class="col-lg-4 col-md-4 col-12">
+     <img src="pic/image3.jpg" class="img-fluid pb-4">
+     </div>
+     <div class="col-lg-4 col-md-4 col-12">
+     <img src="pic/image3.jpg" class="img-fluid pb-4">
+     </div>
+     <div class="col-lg-4 col-md-4 col-12">
+     <img src="pic/image3.jpg" class="img-fluid pb-4">
+     </div>
+     <div class="col-lg-4 col-md-4 col-12">
+     <img src="pic/image3.jpg" class="img-fluid pb-4">
+     </div>
+     <div class="col-lg-4 col-md-4 col-12">
+     <img src="pic/image3.jpg" class="img-fluid pb-4">
+     </div>
+     <div class="col-lg-4 col-md-4 col-12">
+     <img src="pic/image3.jpg" class="img-fluid pb-4">
+     </div>
+     <div class="col-lg-4 col-md-4 col-12">
+     <img src="pic/image3.jpg" class="img-fluid pb-4">
+     </div>
+     </div>
+     </div>
+     </section>
+       
+<section class="my-5">
+     <div class="py-5">
+     <h2 class="text-center"> FORM FILLUP </h2> 
+     </div>  
+     <div class="w-50 m-auto">
+       <form action="userinfo.php" method="post">
+         <div class="form-group">
+           <label>USERNAME</label>
+           <input type="text" name ="user" autocomplete="off" class="form-control">
+           </div>
+
+           <div class="form-group">
+           <label>email</label>
+           <input type="text" name ="email" autocomplete="off" class="form-control">
+           </div>
+           <div class="form-group">
+           <label>mobile</label>
+           <input type="text" name ="mobile" autocomplete="off" class="form-control">
+           </div>
+           <div class="form-group">
+           <label>comment</label>
+           <textarea class="form-control" name="comment">
+            </textarea>
+           </div>
+           <button type="submit" class ="btn btn-success"> submit </button>
+           </form>
+           </div>
+           </section>
+         <footer>
+           <p class="p-3 bg-dark text-white text-center">
+           Dev@TOURS & TRAVELS </P>
+         <foooter>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+
+</body>
+</html>
